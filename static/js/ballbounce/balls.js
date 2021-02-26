4e51f840a12497c37eeb53c445946d790c3a4454
Ulisse Mini <ulisse.mini@gmail.com>
2021-02-25 20:49:50 -0500

    Refactor balls code a bit

--- static/js/ballbounce/balls.js
+++ static/js/ballbounce/balls.js
@@ -36,48 +36,73 @@ function setup() {
   })
 }
 
+function rand(high, low) {
+  return (Math.abs(high) + Math.abs(low)) * Math.random() - low
+}
 
-let radius = 30
-let speed = 3
+const radius = 30
+const speed = 3
 
 let balls = []
 
-function rand(high, low) {
-  return (Math.abs(high) + Math.abs(low))*Math.random() - low
-}
+function Ball(x, y) {
+  return {
+    x: x,
+    y: y,
 
-function mouseClicked(x, y) {
-  for (let i = 0; i < balls.length; i++) {
-    const ball = balls[i];
-    if (Math.sqrt((ball.x-x)**2 + (ball.y-y)**2) < radius) {
-      balls.splice(i, 1)
-      return
-    }
+    xv: rand(speed, -speed),
+    yv: rand(speed, -speed),
+    color: '#000'
   }
-
-  balls.push({x: x, y: y, yv: rand(speed, -speed), xv: rand(speed,-speed), color: '#000'})
 }
 
 function distance(b1, b2) {
-  return Math.sqrt((b1.x-b2.x)**2 + (b1.y-b2.y)**2)
+  return Math.sqrt((b1.x - b2.x) ** 2 + (b1.y - b2.y) ** 2)
 }
 
 function intersecting(b1, b2) {
-  return distance(b1,b2) < radius*2
+  return distance(b1, b2) < radius * 2
+}
+
+function deleteBall(i) {
+  balls.splice(i, 1)
 }
 
 // unit delta to apply to b1, to make it go away from b2
 function delta(b1, b2) {
-  let v = [b1.x - b2.x, b1.y - b2.y]
-  let m = Math.sqrt(v[0]**2 + v[1]**2)
-  v[0] /= m
-  v[1] /= m
+  let d = {x: b1.x - b2.x, y: b1.y - b2.y}
+  let m = Math.sqrt(d.x ** 2 + d.y ** 2)
 
-  return v
+  // normalize vectors
+  d.x /= m
+  d.y /= m
+
+  return d
+}
+
+function handleEdges(b) {
+  if (b.x > w - radius) {b.xv = -speed}
+  if (b.y > h - radius) {b.yv = -speed}
+
+  if (b.x < radius) {b.xv = speed}
+  if (b.y < radius) {b.yv = speed}
 }
 
 ctx.font = 'bold 48px serif'
 
+function mouseClicked(x, y) {
+  const b1 = {x: x, y: y}
+
+  for (let i = 0; i < balls.length; i++) {
+    const b2 = balls[i];
+    if (distance(b1, b2) < radius) {
+      deleteBall(i)
+      return // we deleted a ball, so we don't create a new one
+    }
+  }
+
+  balls.push(Ball(x, y))
+}
 
 function draw(_timestamp) {
   // ctx.clearRect(0,0,w,h)
@@ -85,40 +110,39 @@ function draw(_timestamp) {
   ctx.fillStyle = 'rgba(200,200,200,0.4)'
   ctx.fillRect(0, 0, w, h)
 
-  // ctx.fillRect(x, y, radius, radius)
   for (let ball of balls) {
+    // take the small step according to velocity
     ball.x += ball.xv
     ball.y += ball.yv
-    const x = ball.x; const y = ball.y;
 
-    if (x > w-radius) { ball.xv = -speed + rand(1, -1) }
-    if (y > h-radius) { ball.yv = -speed + rand(1, -1) }
-
-    if (x < radius) { ball.xv = speed + rand(1, -1) }
-    if (y < radius) { ball.yv = speed + rand(1, -1) }
+    // adjust velocity if you hit the sides or top
+    handleEdges(ball)
 
     // check agienst other balls.
     let hit = false;
     for (let b2 of balls) {
       if (b2 !== ball && intersecting(ball, b2)) {
         hit = true;
-        const [dx, dy] = delta(ball, b2)
-        ball.xv = speed*dx
-        ball.yv = speed*dy
+        const d = delta(ball, b2)
+        ball.xv = speed * d.x
+        ball.yv = speed * d.y
       }
     }
     ball.color = hit ? '#f00' : '#000'
   }
 
   for (let ball of balls) {
+    // Draw the balls! note we've already changed color above, when we
+    // checked for collisions
     ctx.beginPath()
     ctx.fillStyle = ball.color
-    ctx.ellipse(ball.x, ball.y, radius, radius, 0, 0, 2*Math.PI)
+    ctx.ellipse(ball.x, ball.y, radius, radius, 0, 0, 2 * Math.PI)
     ctx.fill()
     ctx.stroke()
 
+    // Draw lines between close balls
     // for (let b of balls) {
-    //   if (distance(b, ball) < radius*8) {
+    //   if (distance(b, ball) < radius * 8) {
     //     ctx.beginPath()
     //     ctx.moveTo(ball.x, ball.y)
     //     ctx.lineTo(b.x, b.y)
@@ -128,11 +152,6 @@ function draw(_timestamp) {
   }
 
   window.requestAnimationFrame(draw)
-}
-
-// for testing
-for (let i = 1; i < 10; i++) {
-  mouseClicked(radius*i, radius*i)
 }
 
 setup()
